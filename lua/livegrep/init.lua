01fdfe9f854959079e79ba797ecf5386f6557780
Nathan Smith <nathan@nathansmith.io>
2022-09-03 13:55:42 -0700

    Organize files

---
+++ lua/livegrep/init.lua
@@ -0,0 +1,32 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+
+local M = {}
+
+local entry_maker = function(entry)
+  return {
+    value = entry,
+    display = vim.trim(entry.line),
+    ordinal = vim.trim(entry.line),
+    filename = entry.path,
+    lnum = entry.lno,
+  }
+end
+
+M.livegrep = function(opts)
+  opts = opts or {}
+  pickers
+    .new(opts, {
+      prompt_title = "Livegrep",
+      finder = finders.new_dynamic({
+        fn = require("livegrep.search"),
+        entry_maker = entry_maker,
+      }),
+      sorter = conf.file_sorter(opts),
+      previewer = conf.file_previewer(opts),
+    })
+    :find()
+end
+
+return M
---
+++ lua/livegrep/search.lua
@@ -0,0 +1,27 @@
+local curl = require("plenary.curl")
+
+local default_url = "https://livegrep.com/api/v1/search/linux"
+
+local function search_livegrep(query, params)
+  -- Wait for 2 characters to search
+  if string.len(vim.trim(query)) < 2 then
+    return {}
+  end
+
+  local query_params = {
+    q = query,
+    fold_case = "auto",
+    regex = "true",
+  }
+  local url = params.url or default_url
+
+  local response = curl.get({
+    url = url,
+    accept = "application/json",
+    query = query_params,
+  })
+  local parsed_body = vim.fn.json_decode(response.body)
+  return parsed_body.results
+end
+
+return search_livegrep
